c6af694f735cacbc9cb27639e4cf035a92557e7f
Joey Cengel <t80088@gmail.com>
2020-02-20 19:07:10 -0500

    More testing of expressJS

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-"use strict";
+const express = require('express')
+const app = express()
+const port = 3000
+test = false
 
-console.log("Test");
+app.get('/attempt', (req,res)=>{
+    if(test){
+        res.send('Door Unlocked')
+    }
+    else{
+        res.send('Door Locked')
+    }
+})
+
+app.get('/lever', (req,res)=>{
+    test = !test
+    res.send('Click')
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
